de649da6fd1cb087802a6d748f8567115fb182f6
방현지 <banghyeonji@banghyeonjiui-MacBookPro.local>
2022-07-16 01:03:28 +0900

    ch4. Vue Router 설정까지 완료

---
+++ src/views/HyunjiView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="me">
+    <HyunJi msg="I'm Hyunji"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HyunJi from '@/components/HyunJi.vue'
+
+export default {
+  name: 'HyunjiView',
+  components: {
+    HyunJi
+  }
+}
+</script>
