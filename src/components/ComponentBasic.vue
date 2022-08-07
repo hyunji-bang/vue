88db30bc5e9b577a0057fafb268214b24072dcc9
hyunji-bang <banghyeonji@banghyeonjiui-MacBookPro.local>
2022-08-07 13:50:05 +0900

    vue Basic Component 실습

---
+++ src/components/ComponentBasic.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>도시를 선택해주세요</p>
+    <select name="" id="" v-model="selectCityName" @change="changeCity">
+        <option value="02">서울</option>
+        <option value="031">경기도</option>
+        <option value="051">부산</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutName', // 컴포넌트 이름
+  components: {}, // 다른 컴포넌트 사용 시 컴포넌트를 import하고 배열로 저장
+  data () { // html과 자바스크립트 코드에서 사용할 데이터 변수 선언
+    return {
+      selectCityName: ''
+    }
+  },
+  methods: {
+    changeCity () {
+      console.log(this.selectCityName)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  select {width: 200px; padding:5px}
+</style>
--- src/views/HyunjiView.vue
+++ src/views/HyunjiView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="me">
     <HyunJi msg="I'm Hyunji"/>
+    <ComponentBasic/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HyunJi from '@/components/HyunJi.vue'
+import ComponentBasic from '@/components/ComponentBasic.vue'
 
 export default {
   name: 'HyunjiView',
   components: {
-    HyunJi
+    HyunJi,
+    ComponentBasic
   }
 }
 </script>
